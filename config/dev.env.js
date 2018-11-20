ea10a7685b03effe2b8010f5ee019b9144613d11
koheinaito <naito@plusjam.jp>
2018-11-20 19:59:59 +0900

    envファイルの変更

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  Callname: '"https://script.google.com/macros/s/AKfycbwQKDeTB6tb-RZizRuF1WX1sR2n0Px-mA337sTyf_n0uCxDSw/exec"',
+  Callname2: '"https://script.google.com/macros/s/AKfycbwQKDeTB6tb-RZizRuF1WX1sR2n0Px-mA337sTyf_n0uCxDSw/exec"',
 })
